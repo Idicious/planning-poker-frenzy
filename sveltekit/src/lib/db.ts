e64a7c2373d5bc1a434c575db4ada69d24e6a9d2
David Sherman <thedcsherman@gmail.com>
2022-11-16 19:59:38 +0100

    change to static env variables

--- sveltekit/src/lib/db.ts
+++ sveltekit/src/lib/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 // Create a single supabase client for interacting with your database
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
