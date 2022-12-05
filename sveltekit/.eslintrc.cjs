66bc64f2d1eba7ea2a9b8566beff61c9b3516460
David Sherman <thedcsherman@gmail.com>
2022-12-05 10:33:16 +0100

    configure unused vars eslint config

--- sveltekit/.eslintrc.cjs
+++ sveltekit/.eslintrc.cjs
@@ -16,5 +16,16 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": [
+		"warn", // or "error"
+		{ 
+			"argsIgnorePattern": "^_",
+			"varsIgnorePattern": "^_",
+			"caughtErrorsIgnorePattern": "^_"
+		}
+		],
 	}
 };
