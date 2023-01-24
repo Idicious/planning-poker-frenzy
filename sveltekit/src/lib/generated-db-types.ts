44df74b90a6d2d6c046ddd93f73b3ba460a54343
David Sherman <thedcsherman@gmail.com>
2023-01-25 00:02:19 +0100

    prettier ignore generated types

--- sveltekit/src/lib/generated-db-types.ts
+++ sveltekit/src/lib/generated-db-types.ts
@@ -1,109 +1,116 @@
-export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
 
 export interface Database {
-	public: {
-		Tables: {
-			polls: {
-				Row: {
-					created_at: string | null;
-					description: string | null;
-					id: string;
-					room_id: string | null;
-					votes_visible: boolean;
-				};
-				Insert: {
-					created_at?: string | null;
-					description?: string | null;
-					id?: string;
-					room_id?: string | null;
-					votes_visible?: boolean;
-				};
-				Update: {
-					created_at?: string | null;
-					description?: string | null;
-					id?: string;
-					room_id?: string | null;
-					votes_visible?: boolean;
-				};
-			};
-			profiles: {
-				Row: {
-					avatar_url: string | null;
-					full_name: string | null;
-					id: string;
-					updated_at: string | null;
-					username: string | null;
-					website: string | null;
-				};
-				Insert: {
-					avatar_url?: string | null;
-					full_name?: string | null;
-					id?: string;
-					updated_at?: string | null;
-					username?: string | null;
-					website?: string | null;
-				};
-				Update: {
-					avatar_url?: string | null;
-					full_name?: string | null;
-					id?: string;
-					updated_at?: string | null;
-					username?: string | null;
-					website?: string | null;
-				};
-			};
-			rooms: {
-				Row: {
-					created_at: string | null;
-					host_id: string;
-					id: string;
-					name: string;
-				};
-				Insert: {
-					created_at?: string | null;
-					host_id: string;
-					id?: string;
-					name?: string;
-				};
-				Update: {
-					created_at?: string | null;
-					host_id?: string;
-					id?: string;
-					name?: string;
-				};
-			};
-			votes: {
-				Row: {
-					created_at: string | null;
-					id: string;
-					poll_id: string;
-					user_id: string;
-					vote: string | null;
-				};
-				Insert: {
-					created_at?: string | null;
-					id?: string;
-					poll_id: string;
-					user_id: string;
-					vote?: string | null;
-				};
-				Update: {
-					created_at?: string | null;
-					id?: string;
-					poll_id?: string;
-					user_id?: string;
-					vote?: string | null;
-				};
-			};
-		};
-		Views: {
-			[_ in never]: never;
-		};
-		Functions: {
-			[_ in never]: never;
-		};
-		Enums: {
-			[_ in never]: never;
-		};
-	};
+  public: {
+    Tables: {
+      polls: {
+        Row: {
+          created_at: string | null
+          description: string | null
+          id: string
+          room_id: string | null
+          votes_visible: boolean
+        }
+        Insert: {
+          created_at?: string | null
+          description?: string | null
+          id?: string
+          room_id?: string | null
+          votes_visible?: boolean
+        }
+        Update: {
+          created_at?: string | null
+          description?: string | null
+          id?: string
+          room_id?: string | null
+          votes_visible?: boolean
+        }
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          full_name: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+          website: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+      }
+      rooms: {
+        Row: {
+          created_at: string | null
+          host_id: string
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string | null
+          host_id: string
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string | null
+          host_id?: string
+          id?: string
+          name?: string
+        }
+      }
+      votes: {
+        Row: {
+          created_at: string | null
+          id: string
+          poll_id: string
+          user_id: string
+          vote: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: string
+          poll_id: string
+          user_id: string
+          vote?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          poll_id?: string
+          user_id?: string
+          vote?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
 }
+
