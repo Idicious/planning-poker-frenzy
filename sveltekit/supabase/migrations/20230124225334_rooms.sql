1747fa1f9f903b7fd6e8324a2bc361dcea20bba1
David Sherman <thedcsherman@gmail.com>
2023-01-30 00:15:12 +0100

    formatting

--- sveltekit/supabase/migrations/20230124225334_rooms.sql
+++ sveltekit/supabase/migrations/20230124225334_rooms.sql
@@ -16,7 +16,7 @@ CREATE POLICY "Allow rooms read for authenticated users" ON rooms AS permissive
 
 CREATE POLICY "Allow rooms delete for host" ON rooms AS permissive
     FOR DELETE TO authenticated
-        USING ((auth.uid () = host_id));
+        USING (auth.uid () = host_id);
 
 CREATE POLICY "Allow rooms insert for authenticated users" ON rooms AS permissive
     FOR INSERT TO authenticated
@@ -24,7 +24,7 @@ CREATE POLICY "Allow rooms insert for authenticated users" ON rooms AS permissiv
 
 CREATE POLICY "Allow rooms update for host" ON rooms AS permissive
     FOR UPDATE TO authenticated
-        WITH CHECK ((auth.uid () = host_id));
+        WITH CHECK (auth.uid () = host_id);
 
 -- Return the host_id of a room
 CREATE FUNCTION get_host_id (room_id uuid)
@@ -50,7 +50,7 @@ CREATE TABLE polls (
     created_at timestamp with time zone DEFAULT now(),
     created_by uuid REFERENCES profiles (id) ON DELETE SET NULL,
     room_id uuid REFERENCES rooms (id) ON DELETE CASCADE NOT NULL,
-    "description" text DEFAULT ''::text,
+    description text DEFAULT ''::text,
     votes_visible boolean NOT NULL DEFAULT TRUE
 );
 
@@ -66,19 +66,19 @@ CREATE POLICY "Allow polls insert for authenticated users" ON polls AS permissiv
 
 CREATE POLICY "Allow polls delete for user that created it" ON polls AS permissive
     FOR DELETE TO authenticated
-        USING ((auth.uid () = created_by));
+        USING (auth.uid () = created_by);
 
 CREATE POLICY "Allow polls delete for room host" ON polls AS permissive
     FOR DELETE TO authenticated
-        USING ((auth.uid () = get_host_id (room_id)));
+        USING (auth.uid () = get_host_id (room_id));
 
 CREATE POLICY "Allow polls update for user that created it" ON polls AS permissive
     FOR UPDATE TO authenticated
-        WITH CHECK ((auth.uid () = created_by));
+        WITH CHECK (auth.uid () = created_by);
 
 CREATE POLICY "Allow polls update for room host" ON polls AS permissive
     FOR UPDATE TO authenticated
-        WITH CHECK ((auth.uid () = get_host_id (room_id)));
+        WITH CHECK (auth.uid () = get_host_id (room_id));
 
 -- votes table
 CREATE TABLE votes (
@@ -97,7 +97,7 @@ CREATE POLICY "Allow votes read for authenticated users" ON votes AS permissive
 
 CREATE POLICY "Allow votes delete for user" ON votes AS permissive
     FOR DELETE TO authenticated
-        USING ((auth.uid () = user_id));
+        USING (auth.uid () = user_id);
 
 CREATE POLICY "Allow votes insert for authenticated users" ON votes AS permissive
     FOR INSERT TO authenticated
@@ -105,5 +105,5 @@ CREATE POLICY "Allow votes insert for authenticated users" ON votes AS permissiv
 
 CREATE POLICY "Allow votes update for user" ON votes AS permissive
     FOR UPDATE TO authenticated
-        WITH CHECK ((auth.uid () = user_id));
+        WITH CHECK (auth.uid () = user_id);
 
